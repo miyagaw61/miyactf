99f4e2b452c5047f2de0df2095fe288da0c4c73a
root <miyagaw61@gmail.com>
2017-05-08 11:28:57 +0900

    rm_other_than_binary

--- Rev/CanYouFind/stage01/stage01.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	char flag = "miyactf{Y0uC4nL00k4tStr1ngs}";
-	printf("Can you find the flag?\n");
-	return 0;	
-}
--- Rev/CanYouFind/stage02/stage02.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-	char flag[] = "almiyactf{Y0uC4nL00k4t4rr4y}bc";
-	printf("Can you find the flag?\n");
-	return 0;	
-}
